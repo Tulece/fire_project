b5f030af4a1f6c8b7e445fd23f88bf72770c429e
Tulece <antoine.porceddu@hotmail.ca>
2024-09-18 21:08:21 +0200

    django OK + gitignore

--- detection_incendie/detection_incendie/settings.py
+++ detection_incendie/detection_incendie/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'detection_incendie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'detection/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'detection_incendie.wsgi.application'
 
